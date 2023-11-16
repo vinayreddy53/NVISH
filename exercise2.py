99b3e250b0b3f51d691269b725ecc4fcbdbfb164
vinayreddy53 <65540002+vinayreddy53@users.noreply.github.com>
2023-11-16 18:26:42 +0530

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,1 +1,20 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+# Pre-shared key (this could be loaded from a secure location)
+pre_shared_key = "secret_key"
+
+@app.route('/authorize', methods=['POST'])
+def authorize():
+    # Get the authorization header from the request
+    auth_header = request.headers.get('Authorization')
+
+    # Check if the header matches the pre-shared key
+    if auth_header == pre_shared_key:
+        return jsonify({'message': 'Authorized!'})
+    else:
+        return jsonify({'message': 'Unauthorized'}), 401
+
+if __name__ == '__main__':
+    app.run(debug=True)
